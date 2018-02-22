ad287c79e61a0ee14cc2293a5201d8ffc8a21a02
Dillon Tidgewell <tidge101@mail.chapman.edu>
2018-02-22 12:01:26 -0800

    Get filename from command line

--- bestFirstRobot.cpp
+++ bestFirstRobot.cpp
@@ -1,1 +1,17 @@
-# implement best-first robot search 
+//implement best-first robot search 
+
+#include <stdio.h>;
+#include <>;
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	string filename; 
+	// Get filename from command line inputs
+
+	if(argc < 2){cout << "No file specified in command line\n";}
+	else(){filename = argv[1];}
+	if(argc > 2){cout << "Extra command line input detected";}
+
+	
+}
