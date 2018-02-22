838a7d0f58fb2a59421d7ead9759d57bf7340bdf
Dillon Tidgewell <tidge101@mail.chapman.edu>
2018-02-22 12:04:32 -0800

    Small fixes in includes

--- bestFirstRobot.cpp
+++ bestFirstRobot.cpp
@@ -1,17 +1,18 @@
-//implement best-first robot search 
+//implement best-first robot search
 
 #include <stdio.h>;
-#include <>;
+#include <iostream>;
+#include <fstream>;
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-	string filename; 
+	string filename;
 	// Get filename from command line inputs
 
 	if(argc < 2){cout << "No file specified in command line\n";}
 	else(){filename = argv[1];}
 	if(argc > 2){cout << "Extra command line input detected";}
 
-	
+
 }
