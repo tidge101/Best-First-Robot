a8428c24c015c7ec0ef7af52acf17831cb16e8f1
Dillon Tidgewell <tidge101@mail.chapman.edu>
2018-02-27 11:23:27 -0800

    Trying to clear errors

--- bestFirstRobot.cpp
+++ bestFirstRobot.cpp
@@ -1,20 +1,20 @@
 //implement best-first robot search
 
-#include <stdio.h>;
-#include <string>;
-#include <iostream>;
-#include <fstream>;
-#include <tuple>;
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <tuple>
 
 using namespace std;
 
-vector<vector<char>> parseGrid(string fname){
+vector< vector<char> > parseGrid(string fname){
 	// Parse input file
 	ifstream ifs(fname);
 	string content( (std::istreambuf_iterator<char>(ifs) ),
                   (std::istreambuf_iterator<char>()  ) );
 	int n = content[0];
-	vector<vector<char>> result;
+	vector< vector<char> > result;
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
 			result[i][j] = content[i*n+j+1];
@@ -27,18 +27,18 @@ int main(int argc, char *argv[]){
 	// Get filename from command line inputs
 
 	if(argc < 2){cout << "No file specified in command line\n";}
-	else(){filename = argv[1];}
+	else{filename = argv[1];}
 	if(argc > 2){cout << "Extra command line input detected";}
 
 	// Parse map and store relevant values
 	tuple<int,int> initialState;
 	tuple<int,int> goalState;
-	vector<tuple<int,int>> obstacles;
-	vector<vector<char>> grid;
+	vector< tuple<int,int> > obstacles;
+	vector< vector<char> > grid;
 	int N;
 	// Get dimensions
 	ifstream infile(filename);
-	sting line;
+	string line;
 	getline(infile, line);
 	N = stoi(line);
 
